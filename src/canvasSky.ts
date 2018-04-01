d3a8295eaa069d34fe7efb75f4b1d9faaeee0032
Jonas Wirth <jonas-wirth@gmx.ch>
2018-04-01 18:01:49 +0200

    Refactoring

--- src/canvasSky.ts
+++ src/canvasSky.ts
@@ -1,5 +1,5 @@
 class canvasSky{
-private skySettings:any;
+private settings:any;
 private canvas : HTMLCanvasElement;
 private ctx : CanvasRenderingContext2D;
 
@@ -7,7 +7,7 @@ constructor(elementId:string, configFile:string = null){
   this.canvas = <HTMLCanvasElement> document.getElementById(elementId);
   this.ctx = this.canvas.getContext("2d");
   if(configFile === null){
-    this.getDefaultConfig();
+    this.loadDefaultConfig();
   }else{
     this.loadConfig(configFile);
   }
@@ -16,7 +16,7 @@ constructor(elementId:string, configFile:string = null){
 private loadConfig(configFile:string):void{
   var request = new XMLHttpRequest();
   request.onload = () => {
-    this.skySettings = JSON.parse(request.responseText);
+    this.settings = JSON.parse(request.responseText);
     this.init();
   };
   request.open("get", configFile, true);
@@ -29,7 +29,7 @@ this.canvas.width = this.getWidth();
 this.canvas.height = this.getHeight();
 
 //redraw canvas on window resize if mode = responsive
-if(this.skySettings.mode === 'responsive'){
+if(this.settings.mode === 'responsive'){
   window.addEventListener('resize', () => {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.canvas.width = this.getWidth();
@@ -39,7 +39,7 @@ if(this.skySettings.mode === 'responsive'){
 }
 
 //set background color
-this.canvas.setAttribute("style", "background: linear-gradient("+this.appendColors(this.skySettings.colors)+")");
+this.canvas.setAttribute("style", "background: linear-gradient("+this.appendColors(this.settings.colors)+")");
 
 this.drawStars();
 };
@@ -58,34 +58,34 @@ private appendColors(colors:Array<string>):string{
 private drawStars():void{
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     var j, dot;
-    for(j = 0; j < this.skySettings.stars.count; j++) {
+    for(j = 0; j < this.settings.stars.count; j++) {
       this.ctx.beginPath();
-      this.ctx.arc(Math.random() * this.canvas.width, Math.random() * this.canvas.height, Math.random() * (this.skySettings.stars.minSize - this.skySettings.stars.maxSize + 1) + this.skySettings.stars.minSize, 0, Math.PI * 2, false);
-      this.ctx.fillStyle = this.skySettings.stars.color;
+      this.ctx.arc(Math.random() * this.canvas.width, Math.random() * this.canvas.height, Math.random() * (this.settings.stars.minSize - this.settings.stars.maxSize + 1) + this.settings.stars.minSize, 0, Math.PI * 2, false);
+      this.ctx.fillStyle = this.settings.stars.color;
       this.ctx.fill();
     }
 };
 
 private getHeight():number{
-  if(this.skySettings.size === "fullpage"){
+  if(this.settings.size === "fullpage"){
     return window.innerHeight;
   }
-  if(this.skySettings.size.height.slice(-1) === "%"){
-    return window.innerHeight / 100 * this.skySettings.size.height.substring(0, this.skySettings.size.height.length - 1);
+  if(this.settings.size.height.slice(-1) === "%"){
+    return window.innerHeight / 100 * this.settings.size.height.substring(0, this.settings.size.height.length - 1);
   }
 }
 
 private getWidth():number{
-  if(this.skySettings.size === "fullpage"){
+  if(this.settings.size === "fullpage"){
     return window.innerWidth;
   }
-  if(this.skySettings.size.width.slice(-1) === "%"){
-    return window.innerWidth / 100 * this.skySettings.size.width.substring(0, this.skySettings.size.width.length - 1);
+  if(this.settings.size.width.slice(-1) === "%"){
+    return window.innerWidth / 100 * this.settings.size.width.substring(0, this.settings.size.width.length - 1);
   }
 }
 
-private getDefaultConfig():void{
-this.skySettings = {
+private loadDefaultConfig():void{
+this.settings = {
   size: {
     width: "100%",
     height: "50%"
